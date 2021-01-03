df3f2c97f6ca85834a66aecfa4899c864d9dc188
Your Name <you@example.com>
2021-01-03 10:52:16 +0530

    yupp

---
+++ src/pages/Home.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Home.css";
+function Home() {
+  return (
+    <div>
+      <h1>Let's Build a GOOGLE Clone with REACT JS </h1>
+
+      <div className="home__header">
+        <div className="home__headerLeft">
+          {/* link */}
+          {/* link */}
+        </div>
+
+        <div className="home__headerRight">
+          {/* link */}
+          {/* link */}
+          {/* icon */}
+          {/* avatar */}
+        </div>
+      </div>
+
+      <div className="home__body"></div>
+    </div>
+  );
+}
+
+export default Home;
